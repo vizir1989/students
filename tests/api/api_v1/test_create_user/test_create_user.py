047420277483f1fc53675f075a2cf8153801f4bb
Нинчук Владислав Сергеевич <VSNinchuk@sberbank.ru>
2021-05-30 20:09:36 +0300

    rewrote create_user and login_user tests

--- tests/api/api_v1/test_create_user/test_create_user.py
+++ tests/api/api_v1/test_create_user/test_create_user.py
@@ -1,40 +1,44 @@
-# import os
-#
-# import pytest
-#
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-#
-# TASK_FIXTURE = os.path.join(dir_path, 'fixture')
-#
-#
-# @pytest.mark.parametrize('fixtures_dir, user, expected_code',
-#                          (
-#                                  [
-#                                      TASK_FIXTURE,
-#                                      {
-#                                          'user':
-#                                              {
-#                                                  'username': 'test1',
-#                                                  'email': 'test1@email.com',
-#                                                  'password': '12345678'
-#                                              }
-#                                      },
-#                                      422
-#                                  ],
-#                                  [
-#                                      TASK_FIXTURE,
-#                                      {
-#                                          'user':
-#                                              {
-#                                                  'username': 'test2',
-#                                                  'email': 'test2@email.com',
-#                                                  'password': '12345678'
-#                                              }
-#                                      },
-#                                      201
-#                                  ]
-#                          )
-#                          )
-# def test_create_user(test_client, create_user_fixture, user, expected_code):
-#     response = test_client.post('/api/v1/users', json=user)
-#     assert response.status_code == expected_code
+import os
+
+import pytest
+
+from app.core.config import users_collection_name
+from tests.conftest import find_all
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+TASK_FIXTURE = os.path.join(dir_path, 'fixture')
+
+
+@pytest.mark.parametrize('fixtures_dir, user, expected_code',
+                         (
+                                 [
+                                     TASK_FIXTURE,
+                                     {
+                                         'user':
+                                             {
+                                                 'username': 'test1',
+                                                 'email': 'test1@email.com',
+                                                 'password': '12345678'
+                                             }
+                                     },
+                                     422
+                                 ],
+                                 [
+                                     TASK_FIXTURE,
+                                     {
+                                         'user':
+                                             {
+                                                 'username': 'test2',
+                                                 'email': 'test2@email.com',
+                                                 'password': '12345678'
+                                             }
+                                     },
+                                     201
+                                 ]
+                         )
+                         )
+def test_create_user(test_client, create_user_fixture, user, expected_code):
+    response = test_client.post('/api/v1/users', json=user)
+    assert response.status_code == expected_code
+    assert len(find_all(users_collection_name, {'username': user['user']['username']})) == 1
--- tests/api/api_v1/test_users_login/test_users_login.py
+++ tests/api/api_v1/test_users_login/test_users_login.py
@@ -1,34 +1,40 @@
-# import os
-#
-# import pytest
-#
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-#
-# TASK_FIXTURE = os.path.join(dir_path, 'fixture')
-#
-#
-# @pytest.mark.parametrize('fixtures_dir, user, expected_code',
-#                          (
-#                                  [
-#                                      TASK_FIXTURE,
-#                                      {
-#                                          'username': 'test1',
-#                                          'email': 'test1@email.com',
-#                                          'password': '12345678'
-#                                      },
-#                                      200
-#                                  ],
-#                                  [
-#                                      TASK_FIXTURE,
-#                                      {
-#                                          'username': 'test2',
-#                                          'email': 'test2@email.com',
-#                                          'password': '12345678'
-#                                      },
-#                                      400
-#                                  ]
-#                          )
-#                          )
-# def test_users_login(test_client, users_login_fixture, user, expected_code):
-#     response = test_client.post('/api/v1/users/login', data=user)
-#     assert response.status_code == expected_code
+import os
+
+import pytest
+
+from app.core.config import users_collection_name
+from tests.conftest import find_all
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+TASK_FIXTURE = os.path.join(dir_path, 'fixture')
+
+
+@pytest.mark.parametrize('fixtures_dir, user, expected_code, user_number',
+                         (
+                                 [
+                                     TASK_FIXTURE,
+                                     {
+                                         'username': 'test1',
+                                         'email': 'test1@email.com',
+                                         'password': '12345678'
+                                     },
+                                     200,
+                                     1
+                                 ],
+                                 [
+                                     TASK_FIXTURE,
+                                     {
+                                         'username': 'test2',
+                                         'email': 'test2@email.com',
+                                         'password': '12345678'
+                                     },
+                                     400,
+                                     0
+                                 ]
+                         )
+                         )
+def test_users_login(test_client, users_login_fixture, user, expected_code, user_number):
+    response = test_client.post('/api/v1/users/login', data=user)
+    assert response.status_code == expected_code
+    assert len(find_all(users_collection_name, {'username': user['username']})) == user_number
