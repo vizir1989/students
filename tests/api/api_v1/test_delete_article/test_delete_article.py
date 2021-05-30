b6dd97fccfb4b7b658405adffcf344f0c5a98c79
Нинчук Владислав Сергеевич <VSNinchuk@sberbank.ru>
2021-05-30 20:49:45 +0300

    added update_article tests

--- tests/api/api_v1/test_delete_article/test_delete_article.py
+++ tests/api/api_v1/test_delete_article/test_delete_article.py
@@ -10,36 +10,36 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 TASK_FIXTURE = os.path.join(dir_path, 'fixture')
 
 
-@pytest.mark.parametrize('fixtures_dir, slug, token, expected_code, article_before, article_after',
+@pytest.mark.parametrize('fixtures_dir, token, slug, expected_code, article_before, article_after',
                          (
                                  [
                                      TASK_FIXTURE,
+                                     'Bearer test1',
                                      'title-1',
-                                     "Bearer test1",
                                      204,
                                      1,
                                      0
                                  ],
                                  [
                                      TASK_FIXTURE,
-                                     'title-2',
                                      'Bearer test1',
+                                     'title-2',
                                      404,
                                      0,
                                      0
                                  ],
                                  [
                                      TASK_FIXTURE,
-                                     'title-1',
                                      'Bearer test2',
+                                     'title-1',
                                      404,
                                      1,
                                      1
                                  ]
                          )
                          )
-def test_delete_article_fixture(test_client, delete_article_fixture, slug, token, expected_code, article_before,
-                                article_after):
+def test_delete_article(test_client, delete_article_fixture, token, slug, expected_code, article_before,
+                        article_after):
     assert len(find_all(article_collection_name, {'slug': slug})) == article_before
     response = test_client.delete(f'/api/v1/articles/{slug}', headers={'Authorization': token})
     assert response.status_code == expected_code
---
+++ tests/api/api_v1/test_update_articles/test_update_article.py
@@ -0,0 +1,56 @@
+import os
+
+import pytest
+from slugify import slugify
+
+from app.core.config import article_collection_name
+from tests.conftest import find_all
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+TASK_FIXTURE = os.path.join(dir_path, 'fixture')
+
+
+@pytest.mark.parametrize('fixtures_dir, token, article, expected_code, check_article',
+                         (
+                                 [
+                                     TASK_FIXTURE,
+                                     'Bearer test1',
+                                     {
+                                         'article':
+                                             {
+                                                 'title': 'title 1',
+                                                 'description': 'description 2',
+                                                 'body': 'body 2',
+                                                 'tag_list': ['tag 1', 'tag 3']
+                                             }
+                                     },
+                                     200,
+                                     True
+                                 ],
+                                 [
+                                     TASK_FIXTURE,
+                                     'Bearer test2',
+                                     {
+                                         'article':
+                                             {
+                                                 'title': 'title 1',
+                                                 'description': 'description 2',
+                                                 'body': 'body 2',
+                                                 'tag_list': ['tag 1', 'tag 3']
+                                             }
+                                     },
+                                     404,
+                                     False
+                                 ]
+                         )
+                         )
+def test_update_article(test_client, update_article_fixture, token, article, expected_code, check_article):
+    slug = slugify(article['article']['title'])
+    response = test_client.put(f'/api/v1/articles/{slug}', headers={'Authorization': token}, json=article)
+    assert response.status_code == expected_code
+    if check_article:
+        result = find_all(article_collection_name, {'title': article['article']['title']})
+        assert len(result) == 1
+        for k, v in article['article'].items():
+            assert result[0][k] == v
